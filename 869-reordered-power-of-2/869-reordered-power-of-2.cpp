420b8cd2bb33644910a1cd7b3f3dbbdaf1b1297c
Garima Jain <84665079+garimajain12@users.noreply.github.com>
2022-08-26 15:49:56 +0530

    Time: 8 ms (35.03%), Space: 6 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -3,7 +3,7 @@ public:
     bool reorderedPowerOf2(int n) {
         string str=to_string(n);
         sort(str.begin(), str.end());
-        for(int i=0;i<30;i++){
+        for(int i=0;i<31;i++){
             string temp=to_string(1<<i);
             sort(temp.begin(), temp.end());
             if(str==temp){
