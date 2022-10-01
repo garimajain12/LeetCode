0d5e4117aaa7b4b6992d4f0003db8b048eae905d
Garima Jain <84665079+garimajain12@users.noreply.github.com>
2022-10-01 21:50:31 +0530

    Time: 50 ms (72.10%), Space: 15.3 MB (57.83%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.cpp
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -5,12 +5,12 @@ public:
         vector<vector<int>> ans;
         for(auto it:build)
         {
-            vt.push_back({it[0],-it[2]}); //leftWall
-            vt.push_back({it[1],it[2]}); //rightWall
+            vt.push_back({it[0],-it[2]}); 
+            vt.push_back({it[1],it[2]}); 
         }
         sort(vt.begin(),vt.end());
         multiset<int> mt{0};
-        int top = 0; // record the height of skyLine
+        int top = 0; 
         for(auto itr:vt)
         {
             if(itr.second < 0)
@@ -21,9 +21,8 @@ public:
             {
                 mt.erase(mt.find(itr.second));
             }
-            if(*mt.rbegin() != top) // when top height changes then 
+            if(*mt.rbegin() != top) 
             {
-                // cout<<itr.second<<" "<<top<<" ";
                 top = *mt.rbegin();
                 ans.push_back({itr.first,top});
             }
